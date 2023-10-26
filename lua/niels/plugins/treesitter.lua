1fbc4455b3cbbe827bb26c707525d4c1e31fbba6
Niels-py <niels22haupt@gmail.com>
2023-10-27 00:02:52 +0200

    removing after/

--- lua/niels/plugins/treesitter.lua
+++ lua/niels/plugins/treesitter.lua
@@ -4,10 +4,33 @@ return {
     config = function()
         local configs = require('nvim-treesitter.configs')
         configs.setup({
-            ensure_installed = { 'bash', 'lua', 'python', 'rust', 'go', 'c', 'nix' },
+            ensure_installed = { 
+                'bash',
+                'lua',
+                'python',
+                'rust',
+                'go',
+                'c',
+                'nix',
+                'html',
+                'javascript',
+                'json',
+                'markdown',
+                'markdown_inline',
+                'tsx',
+                'typescript',
+                'yaml',
+                'rust',
+                'cpp',
+                'css',
+                'csv',
+                'sql'
+            },
+
             sync_install = false,
             highlight = { enable = true },
             indent = { enable = true },
+            auto_install = true,
         })
     end,
 }
