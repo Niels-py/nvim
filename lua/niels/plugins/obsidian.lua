4548a4a6db1605b0b454fc9c38e487227da041b6
Niels-py <niels22haupt@gmail.com>
2024-01-12 18:19:57 +0100

    added tab complition

---
+++ lua/niels/plugins/obsidian.lua
@@ -0,0 +1,27 @@
+return {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    event = {
+        -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+        -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+        "BufReadPre ~/Documents/notes/**.md",
+        "BufNewFile ~/Documents/notes/**.md",
+    },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "hrsh7th/nvim-cmp",
+        "nvim-telescope/telescope.nvim",
+        "nvim-treesitter",
+    },
+    opts = {
+        workspaces = {
+            {
+                name = "notes",
+                path = "~/Documents/notes",
+            },
+        },
+
+        -- see below for full list of options 👇
+    },
+}
