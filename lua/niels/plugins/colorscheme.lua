7b89974ac1b747451e7b560ebc5c931080657c42
Niels-py <niels22haupt@gmail.com>
2024-05-19 05:53:40 +0200

    dont know why i had fidget and noice together but it does not make any sence

--- lua/niels/plugins/colorscheme.lua
+++ lua/niels/plugins/colorscheme.lua
@@ -1,55 +1,10 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-        require("catppuccin").setup({
-            flavour = "mocha",
-            transparent_background = true,
-            term_colors = true,
-            compile = {
-                enabled = false,
-                path = vim.fn.stdpath("cache") .. "/catppuccin",
-            },
-            dim_inactive = {
-                enabled = false,
-                shade = "dark",
-                percentage = 0.15,
-            },
-            styles = {
-                comments = { "italic" },
-                conditionals = { "italic" },
-                loops = { "bold" },
-                functions = { "italic" },
-                keywords = { "bold" },
-                strings = {},
-                variables = {},
-                numbers = {},
-                booleans = {},
-                properties = {},
-                types = {},
-                operators = {},
-            },
-            integrations = {
-                cmp = true,
-                gitsigns = true,
-                nvimtree = true,
-                treesitter = true,
-                markdown = true,
-                native_lsp = {
-                    enabled = true,
-                    underlines = {
-                        errors = { "undercurl" },
-                        hints = { "undercurl" },
-                        warnings = { "undercurl" },
-                        information = { "undercurl" },
-                    },
-                }
-            },
-            color_overrides = {},
-            highlight_overrides = {},
-        })
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup()
 
-        vim.cmd([[colorscheme catppuccin]])
-    end,
+		vim.cmd([[colorscheme catppuccin]])
+	end,
 }
--- lua/niels/plugins/oil.lua
+++ lua/niels/plugins/oil.lua
@@ -1,6 +1,30 @@
 return {
-    'stevearc/oil.nvim',
-    opts = {},
-    -- Optional dependencies
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+	"stevearc/oil.nvim",
+	-- Optional dependencies
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		delete_to_trash = true,
+		skip_confirm_for_simple_edits = false,
+
+		keymaps = {
+			["g?"] = "actions.show_help",
+			["<CR>"] = "actions.select",
+			["<C-s>"] = "actions.select_vsplit",
+			["<C-h>"] = "actions.select_split",
+			["<C-t>"] = "actions.select_tab",
+			["<C-p>"] = "actions.preview",
+			["<C-c>"] = "actions.close",
+			["<C-l>"] = "actions.refresh",
+			["-"] = "actions.parent",
+			["_"] = "actions.open_cwd",
+			["`"] = "actions.cd",
+			["~"] = "actions.tcd",
+			["gs"] = false, -- remove keymap for leap compatability
+			["gx"] = "actions.open_external",
+			["g."] = "actions.toggle_hidden",
+			["gt"] = "actions.toggle_trash",
+		},
+		-- to use the keymaps keys
+		use_default_keymaps = false,
+	},
 }
