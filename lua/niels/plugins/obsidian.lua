45e196bb06c84ba719bf3c3b02de885044f19243
Niels-py <niels22haupt@gmail.com>
2024-09-09 13:15:17 +0200

    changed Obsidian to be less

--- lua/niels/plugins/obsidian.lua
+++ lua/niels/plugins/obsidian.lua
@@ -26,16 +26,16 @@ return {
 		-- Optional, if you keep notes in a specific subdirectory of your vault.
 		notes_subdir = "notes",
 
-		daily_notes = {
-			-- Optional, if you keep daily notes in a separate directory.
-			folder = "notes/dailies",
-			-- Optional, if you want to change the date format for the ID of daily notes.
-			date_format = "%Y-%m-%d",
-			-- Optional, if you want to change the date format of the default alias of daily notes.
-			alias_format = "%B %-d, %Y",
-			-- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-			template = "daily.md",
-		},
+		-- daily_notes = {
+		-- 	-- Optional, if you keep daily notes in a separate directory.
+		-- 	folder = "notes/dailies",
+		-- 	-- Optional, if you want to change the date format for the ID of daily notes.
+		-- 	date_format = "%Y-%m-%d",
+		-- 	-- Optional, if you want to change the date format of the default alias of daily notes.
+		-- 	alias_format = "%B %-d, %Y",
+		-- 	-- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+		-- 	template = "daily.md",
+		-- },
 
 		-- way then set 'mappings = {}'.
 		mappings = {
@@ -71,13 +71,13 @@ return {
 		disable_frontmatter = true,
 
 		-- Optional, for templates (see below).
-		templates = {
-			subdir = "templates",
-			date_format = "%Y-%m-%d",
-			time_format = "%H:%M",
-			-- A map for custom variables, the key should be the variable and the value a function
-			substitutions = {},
-		},
+		-- templates = {
+		-- 	subdir = "templates",
+		-- 	date_format = "%Y-%m-%d",
+		-- 	time_format = "%H:%M",
+		-- 	-- A map for custom variables, the key should be the variable and the value a function
+		-- 	substitutions = {},
+		-- },
 
 		-- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
 		-- URL it will be ignored but you can customize this behavior here.
@@ -114,8 +114,8 @@ return {
 				-- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
 				[" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
 				["x"] = { char = "", hl_group = "ObsidianDone" },
-				[">"] = { char = "", hl_group = "ObsidianRightArrow" },
-				["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+				-- [">"] = { char = "", hl_group = "ObsidianRightArrow" },
+				-- ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
 				-- Replace the above with this if you don't have a patched font:
 				-- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
 				-- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
