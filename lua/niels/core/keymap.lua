f3d1ec24d57ca27ef823381889913a4e18785761
Niels Haupt <nielshaupt@MacBookAir.fritz.box>
2025-01-20 22:32:46 +0100

    added something i guess

--- lua/niels/core/keymap.lua
+++ lua/niels/core/keymap.lua
@@ -1,6 +1,6 @@
 -- moving selection on line up or down with line indent
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "move selected line down" })
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "move selected line up" })
+-- vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "move selected line down" })
+-- vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "move selected line up" })
 
 -- clear highlight of search by pressing ESC
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
@@ -12,9 +12,3 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 -- vim.keymap.set("n", "<C-k>", "<C-w><C-k>", "Move focus to the upper window" )
 
 -- vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], "replace word under cursor in whole file")
-
--- tabs
--- vim.keymap.set("n", "<leader>t", vim.cmd.tabnew, "Opens new tab")
--- vim.keymap.set("n", "<leader>w", vim.cmd.tabclose, "Closes current tab")
--- vim.keymap.set("n", "<leader><tab>", vim.cmd.tabnext, "switch to next tab")
--- vim.keymap.set("n", "<leader><S-tab>", vim.cmd.tabprevious, "switch to prev tab")
