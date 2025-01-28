3d7b1c3e233051de035ca0e6098707a517e39ac7
Niels Haupt <nielshaupt@Nielss-MacBook-Air.local>
2025-01-28 23:53:12 +0100

    switch cmp blink

--- lua/niels/plugins/conform.lua
+++ lua/niels/plugins/conform.lua
@@ -1,43 +1,55 @@
 return {
 	"stevearc/conform.nvim",
-	event = { "BufWritePre" },
+	event = { "BufReadPre", "BufNewFile" },
 	cmd = { "ConformInfo" },
-	keys = {
-		{
-			-- Customize or remove this keymap to your liking
-			"<leader>F",
-			function()
-				require("conform").format({ async = true, lsp_fallback = true })
-			end,
-			mode = "n",
-			desc = "Format buffer",
-		},
-	},
-	opts = {
-		formatters_by_ft = {
-			lua = { "stylua" },
-			python = function(bufnr)
-				if require("conform").get_formatter_info("ruff_format", bufnr).available then
-					return { "ruff_format" }
-				else
-					return { "isort", "black" }
-				end
-			end,
-			go = { "goimports", "gofmt" },
-			bash = { "beautysh" },
-			rust = { "rustfmt", lsp_format = "fallback" },
-			toml = { "taplo" },
-			zig = { "zigfmt" },
-			json = { "jq" },
-			-- c = { "clangd" },
-			-- cpp = { "clangd" },
-			-- Use the "*" filetype to run formatters on all filetypes.
-			-- ["*"] = { "codespell" },
-		},
-		format_on_save = {
-			-- These options will be passe to conform.format()
-			timeout_ms = 500,
-			lsp_fallback = true,
-		},
-	},
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				svelte = { "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				graphql = { "prettier" },
+				liquid = { "prettier" },
+				lua = { "stylua" },
+				python = function(bufnr)
+					if require("conform").get_formatter_info("ruff_format", bufnr).available then
+						return { "ruff_format" }
+					else
+						return { "isort", "black" }
+					end
+				end,
+				go = { "goimports", "gofmt" },
+				bash = { "beautysh" },
+				rust = { "rustfmt", lsp_format = "fallback" },
+				toml = { "taplo" },
+				zig = { "zigfmt" },
+				-- c = { "clangd" },
+				-- cpp = { "clangd" },
+				-- Use the "*" filetype to run formatters on all filetypes.
+				-- ["*"] = { "codespell" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			},
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>F", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
 }
--- lua/niels/plugins/mason.lua
+++ lua/niels/plugins/mason.lua
@@ -1,8 +1,10 @@
 return {
 	"williamboman/mason.nvim",
 	dependencies = {
+		"neovim/nvim-lspconfig",
 		"williamboman/mason-lspconfig.nvim",
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		-- "rshkarin/mason-nvim-lint",
 	},
 	event = "VeryLazy",
 	config = function()
@@ -46,6 +48,7 @@ return {
 			marksman = {},
 			zls = {},
 			jdtls = {},
+			-- hls = {},
 			lua_ls = {
 				settings = {
 					Lua = {
@@ -68,10 +71,18 @@ return {
 			},
 		}
 
-		-- make ensure_installed with all servers from servers variable
-		local ensure_installed = vim.tbl_keys(servers or {})
-		-- add additional stuff to install
-		vim.list_extend(ensure_installed, {
+		local linters = {
+			"eslint",
+			"selene",
+			"golangci-lint",
+			-- "hlint",
+			"vale",
+			"yamllint",
+			"clang-format",
+		}
+
+		local formatters = {
+
 			"stylua", -- Used to format Lua code
 			"black",
 			"ruff",
@@ -79,11 +90,21 @@ return {
 			"taplo",
 			"beautysh",
 			"stylelint",
-			"jq",
+			-- "jq",
 			"codelldb",
 			"codespell",
 			"goimports",
-		})
+		}
+
+		-- make ensure_installed with all servers from servers variable
+		local ensure_installed = vim.tbl_keys(servers or {})
+
+		-- adding linters and formatters to get installed as well
+		vim.list_extend(ensure_installed, linters)
+		vim.list_extend(ensure_installed, formatters)
+
+		-- setup nvim lint to use mason linter
+		-- require("mason-nvim-lint").setup()
 
 		-- install everything
 		require("mason-tool-installer").setup({
@@ -97,7 +118,7 @@ return {
 			handlers = {
 				function(server_name)
 					local server = servers[server_name] or {}
-					server.capabilities = require("cmp_nvim_lsp").default_capabilities()
+					server.capabilities = require("blink.cmp").get_lsp_capabilities(server.capabilities)
 					require("lspconfig")[server_name].setup(server)
 				end,
 			},
