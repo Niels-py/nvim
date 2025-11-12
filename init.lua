fdf5ccaa047fce934ddefb8003d845e7c7995612
Niels Haupt <niels22haupt@gmail.com>
2025-11-12 03:19:28 +0100

    auto update lazy

--- init.lua
+++ init.lua
@@ -588,6 +588,14 @@ require("lazy").setup({
   checker = { enabled = true },
 })
 
+vim.api.nvim_create_autocmd(
+  "VimEnter", {
+    callback = function()
+      require("lazy").update({ show = false })
+    end
+  }
+)
+
 -- folding autocommand
 -- needs to be after lazy to ensure that treesitter is installed
 vim.api.nvim_create_autocmd({ "FileType" }, {
